8c8020df76a3da1384ccb444764e1ced1bad5403
Aaron Hargrove <aaronmh98@outlook.com>
2020-03-31 16:55:06 -0500

    Fixed form, added some general structure to get off the ground.

---
+++ Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_gui.Controllers
+{
+    // This is for controlling general game logic
+    class GameController
+    {
+    }
+
+    // Get input from player1 / player2 (via AIController or UserController)
+
+    // Determine if anyone has won
+
+    // Update GameBoard
+    
+    // ...  ?
+}
--- Game.cs
+++ Game.cs
@@ -10,11 +10,26 @@ using System.Windows.Forms;
 
 namespace game_gui
 {
-    public partial class Form1 : Form
+    public partial class Game : Form
     {
-        public Form1()
+        public Game()
         {
             InitializeComponent();
         }
+
+        // This is where our event handlers will be for our form. Pass off game control functoinality to controllers
+
+        // C# Conventions: https://github.com/ktaranov/naming-convention/blob/master/C%23%20Coding%20Standards%20and%20Naming%20Conventions.md
+        // Class Names: PascalCase
+        // Constructors: PascalCase
+        // Method Names: PascalCase
+        // Constants: PascalCase
+        // Class Variables: PacalCase
+        // Local Variabls: camelCase
+        // Method Params: camelCase
+
+        // Controllers Folder: For general game control and getting data from users
+        // POCSO (plain old C# objects): If you need a general object, place it here, but I suspect we will have our "board" array just in GameController
+
     }
 }
